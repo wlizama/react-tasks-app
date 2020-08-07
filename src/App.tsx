d1f428c54432c818651d46d42cf523df388a80b5
Wilder Lizama <wilder.lizama@gnt.pe>
2020-08-07 00:49:42 -0500

    Typescript simple task with react js

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,87 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from "react";
 
-type FormElement = React.FormEvent<HTMLFormElement>
+type FormElement = React.FormEvent<HTMLFormElement>;
 interface ITask {
 	name: string;
-	done: boolean
+	done: boolean;
 }
 
 function App() {
-
-	const [newTask, setNewTask] = useState<string>('')
-	const [tasks, setTasks] = useState<ITask[]>([])
+	const [newTask, setNewTask] = useState<string>("");
+	const [tasks, setTasks] = useState<ITask[]>([]);
+	const taskInput = useRef<HTMLInputElement>(null);
 
 	const handleSubmit = (e: FormElement) => {
-		e.preventDefault()
-		addTask(newTask)
-		setNewTask('')
-	}
+		e.preventDefault();
+		addTask(newTask);
+		setNewTask("");
+		taskInput.current?.focus();
+	};
 
-	const addTask = (name: string) =>{
-		const newTask = [...tasks, { name, done: false }]
-		setTasks(newTask)
-	}
+	const addTask = (name: string): void => {
+		const newTask = [...tasks, { name, done: false }];
+		setTasks(newTask);
+	};
+
+	const toggleDoneTask = (i: number): void => {
+		const newTasks: ITask[] = [...tasks];
+		newTasks[i].done = !newTasks[i].done;
+		setTasks(newTasks);
+	};
+
+	const removeTask = (i: number) => {
+		const newTasks: ITask[] = [...tasks];
+		newTasks.splice(i, 1);
+		setTasks(newTasks);
+	};
 
 	return (
-		<>
-			<form onSubmit={handleSubmit}>
-				<input type="text" onChange={e => setNewTask(e.target.value)} value={newTask} />
-				<button>
-					Save
-				</button>
-			</form>
-		</>
+		<div className="container p-4">
+			<div className="row">
+				<div className="col-md-6 offset-md-3">
+					<div className="card">
+						<div className="card-body">
+							<form onSubmit={handleSubmit}>
+								<input
+									autoFocus
+									type="text"
+									ref={taskInput}
+									onChange={(e) => setNewTask(e.target.value)}
+									value={newTask}
+									className="form-control"
+								/>
+								<button className="btn btn-success btn-block mt-2">
+									Save
+								</button>
+							</form>
+						</div>
+					</div>
+					{tasks.map((t: ITask, i: number) => (
+						<div
+							key={i}
+							className="card card-body mt-2 d-flex flex-row justify-content-between"
+						>
+							<h2
+								style={{
+									textDecoration: t.done
+										? "line-through"
+										: "",
+								}}
+								onClick={() => toggleDoneTask(i)}
+							>
+								{t.name}
+							</h2>
+							<button
+								className="btn btn-danger"
+								onClick={() => removeTask(i)}
+							>
+								×
+							</button>
+						</div>
+					))}
+				</div>
+			</div>
+		</div>
 	);
 }
 
